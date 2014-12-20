997e345caeb3d5c030517393b698830759f48237
tanem <tane.morgan@gmail.com>
2014-12-20 20:52:11 +1300

    Add Handlebars dependency

--- test/hbs-svg.test.js
+++ test/hbs-svg.test.js
@@ -22,6 +22,7 @@ test('should write the specified Handlebars helper file', function(t){
       if (err) t.fail(err);
       t.equal(
         data.toString(),
+        'var Handlebars = require(\'handlebars\');\n\n' +
         'Handlebars.registerHelper(\'newspaper.svg\', function(){\n' +
         '  return \'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path d="M28 8V4H0v22c0 1.105.895 2 2 2h27c1.657 0 3-1.343 3-3V8h-4zm-2 18H2V6h24v20zM4 10h20v2H4zm12 4h8v2h-8zm0 4h8v2h-8zm0 4h6v2h-6zM4 14h10v10H4z"/></svg>\';\n' +
         '});\n\n' +
